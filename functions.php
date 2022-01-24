140304f46a524c2ae5eb9429b7819c7e69fde0ba
sarahimm <76983213+sarahimm@users.noreply.github.com>
2022-01-23 16:49:14 -0800

    Added superheader and footer

--- functions.php
+++ functions.php
@@ -21,3 +21,60 @@ function hello_elementor_child_enqueue_scripts() {
 	);
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts');
+
+/* Superheader */
+add_action('wp_head', 'kcp_superheader');
+function kcp_superheader(){
+?>
+	<div id="superheader">
+		<div class="links">
+			<a href="/students">Students</a>
+			<a href="/faculty">Faculty</a>
+		</div>
+	</div>
+<?php
+};
+
+/* Describe what the code snippet does so you can remember later on */
+add_action('wp_footer', 'kcp_footer');
+function kcp_footer(){
+?>
+<div id="kcp-footer">
+  <div class="col col1">
+          <img src="">
+  </div>
+    <div class="col col2">
+      <h2>About</h2>
+      <ul>
+        <li><a href="">Mission</a></li>
+        <li><a href="">History</a></li>
+        <li><a href="">Resources for Students</a></li>
+        <li><a href="">Resources for Faculty</a></li>
+      </ul>
+    </div>
+    <div class="col col3">
+      <h2>Grants</h2>
+      <ul>
+        <li><a href="">Information</a></li>
+        <li><a href="">Eligible Service Programs</a></li>
+        <li><a href="">Apply</a></li>
+      </ul>
+  </div>
+    <div class="col col4">
+      <h2>Contact</h2>
+      <ul>
+        <li><a href="">1234 Address Here<br>Spokane, WA 99251</a></li>
+        <li><a href=""></a>(###)###-####</li>
+        <li><a href="">example@example.com</a></li>
+        <li><a href="">@instagram-handle</a></li>
+      </ul>          
+  </div>
+</div>
+<div id="kcp-superfooter">
+  	<p>
+      &copy; 2022 Krista Colleagues Global Service Grants Program.
+      <a href="/privacy-policy">Privacy Policy</a>
+  </p>
+</div>
+<?php
+};
