01fcd3101dcbffebbab81ea78a1cc28219143b84
sarahimm <76983213+sarahimm@users.noreply.github.com>
2022-01-24 02:10:47 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -28,8 +28,8 @@ function kcp_superheader(){
 ?>
 	<div id="kcp_superheader">
 		<div class="links">
-			<a href="/students">Students</a>
-			<a href="/faculty">Faculty</a>
+			<a href="/student-resources">Students</a>
+			<a href="/faculty-information">Faculty</a>
 		</div>
 	</div>
 <?php
@@ -41,30 +41,29 @@ function kcp_footer(){
 ?>
 <div id="kcp-footer">
   <div class="col col1">
-          <img src="">
+          <img src="http://35.85.213.9/wp-content/uploads/2022/01/KCstackedplurallogo.png" style="max-width: 200px;">
   </div>
     <div class="col col2">
       <h2>About</h2>
       <ul>
-        <li><a href="">Mission</a></li>
-        <li><a href="">History</a></li>
-        <li><a href="">Resources for Students</a></li>
-        <li><a href="">Resources for Faculty</a></li>
+        <li><a href="/about-us">About us</a></li>
+        <li><a href="/student-resources">Resources for Students</a></li>
+        <li><a href="/faculty-information">Resources for Faculty</a></li>
       </ul>
     </div>
     <div class="col col3">
       <h2>Grants</h2>
       <ul>
-        <li><a href="">Information</a></li>
-        <li><a href="">Eligible Service Programs</a></li>
-        <li><a href="">Apply</a></li>
+        <li><a href="/the-grant">Information</a></li>
+        <li><a href="/service-placements">Eligible Service Programs</a></li>
+        <li><a href="/apply">Apply</a></li>
       </ul>
   </div>
     <div class="col col4">
       <h2>Contact</h2>
       <ul>
         <li><a href="">1234 Address Here<br>Spokane, WA 99251</a></li>
-        <li><a href=""></a>(###)###-####</li>
+        <li><a href="">(###)###-####</a></li>
         <li><a href="">example@example.com</a></li>
         <li><a href="">@instagram-handle</a></li>
       </ul>          
